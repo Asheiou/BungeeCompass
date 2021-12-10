4fd277aa537a3981b4db844fe6f9b6eb7bb5f93c
Asheiou <declanthawley@gmail.com>
2021-12-10 21:03:19 +0000

    Remove unused imports

--- src/main/java/uk/asheiou/bungeecompass/PlayerInteractEventListener.java
+++ src/main/java/uk/asheiou/bungeecompass/PlayerInteractEventListener.java
@@ -1,15 +1,11 @@
 package uk.asheiou.bungeecompass;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerInteractEventListener extends CompassGUI implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
