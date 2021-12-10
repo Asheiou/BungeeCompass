df934a14e85cdf9fb13b2f0f5254fd87e50130e4
Asheiou <declanthawley@gmail.com>
2021-12-10 21:04:00 +0000

    Remove unused imports

--- src/main/java/uk/asheiou/bungeecompass/MenuCommandExecutor.java
+++ src/main/java/uk/asheiou/bungeecompass/MenuCommandExecutor.java
@@ -5,7 +5,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class MenuCommandExecutor extends CompassGUI implements CommandExecutor {
     @Override
