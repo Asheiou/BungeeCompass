29159ebb8326136a09c0620396aed636185beaf7
Asheiou <declanthawley@gmail.com>
2021-12-12 12:39:50 +0000

    Create a config.yml because why wouldn't I?

--- src/main/java/uk/asheiou/bungeecompass/BungeeCompass.java
+++ src/main/java/uk/asheiou/bungeecompass/BungeeCompass.java
@@ -2,20 +2,27 @@ package uk.asheiou.bungeecompass;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class BungeeCompass extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        if(!new File(getDataFolder(), "config.yml").exists()) {
+            saveDefaultConfig();
+            getLogger().info("Config file not found! Creating one.");
+        }
         // Register BungeeCord Plugin Channel
         getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         getLogger().info("BungeeCord PluginChannel registered.");
         // Register events
-        getServer().getPluginManager().registerEvents(new EventListeners(), this);
+        getServer().getPluginManager().registerEvents(new EventListeners(this), this);
         getLogger().info("Events registered.");
         // Register commands
         this.getCommand("givecompass").setExecutor(new GiveCompassCommandExecutor());
         this.getCommand("menu").setExecutor(new MenuCommandExecutor());
         getLogger().info("Load complete.");
+
     }
     @Override
     public void onDisable() {
